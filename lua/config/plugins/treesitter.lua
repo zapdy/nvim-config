e457fc1ffc9bcb202b471d9ce265ad02d8a1031e
= <zapdyy@gmail.com>
2026-03-11 02:15:55 +0100

    change onedark theme

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     lazy = false,
     build = ':TSUpdate',
     config = function()
-        local languages = { "lua" }
+        local languages = { "lua", "c", "cpp" }
         require'nvim-treesitter'.install(languages) 
 
         -- highlighting
@@ -11,10 +11,6 @@ return {
             pattern = languages,
             callback = function() vim.treesitter.start() end,
         })
-
-        -- folds
-        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        vim.wo[0][0].foldmethod = 'expr'
 
         -- indentation
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
